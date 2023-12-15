ffbe9daec6e9310f57dc1182ecca489caaba2e46
Javier <jimenez.franco.francisco.javier@iescamas.es>
2023-12-15 14:12:17 +0100

    subida de carpeta php

---
+++ php/conexion_be.php
@@ -0,0 +1,13 @@
+<?php
+
+$conexion = mysqli_connect("localhost", "myexamende6f", "E2vI94rq", "loginavanzado1824182414");
+
+/*/
+if ($conexion){
+    echo "conectado";
+}else{
+        echo "no conectado";
+}
+*/
+
+?>
